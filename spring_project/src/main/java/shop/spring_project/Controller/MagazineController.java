23230d9cc5e0c5b452e7eec65eeb066dbf072944
kimagine94 <ovoa22@gmail.com>
2022-10-25 12:28:04 +0900

    [feat] Magazine 관련 기본 api 완성 - edit/delete 서비스 추가 - exception 처리 - 오탈자 수정

--- spring_project/src/main/java/shop/spring_project/Controller/MagazineController.java
+++ spring_project/src/main/java/shop/spring_project/Controller/MagazineController.java
@@ -2,13 +2,15 @@ package shop.spring_project.Controller;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shop.spring_project.DTO.MagazineDTO;
 import shop.spring_project.Entity.Magazine;
 import shop.spring_project.Service.MagazineService;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 
 @RestController
 public class MagazineController {
@@ -18,7 +20,6 @@ public class MagazineController {
 
     @GetMapping("/magazine/read/{id}")
     public MagazineDTO getOneMagazine(@PathVariable("id") Long id){
-        //return magazineService.getMagazine(id);
         MagazineDTO magazineDTO = magazineService.getMagazine(id);
         return  magazineDTO;
     }
@@ -35,4 +36,12 @@ public class MagazineController {
 
     @DeleteMapping("/magazine/delete/{id}")
     public Long removeMagazine(@PathVariable("id") Long id){ return  magazineService.deleteMagazine(id); }
+
+    @PutMapping("/magazine/edit")
+    public Long updateMagazine(@RequestBody Magazine magazine) { return  magazineService.editMagazine(magazine); }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementFoundException(NoSuchElementException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
 }
--- spring_project/src/main/java/shop/spring_project/Service/MagazineService.java
+++ spring_project/src/main/java/shop/spring_project/Service/MagazineService.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.spring_project.DTO.MagazineDTO;
 import shop.spring_project.Entity.Magazine;
-import shop.spring_project.Mapper.MagagineMapper;
+import shop.spring_project.Mapper.MagazineMapper;
 import shop.spring_project.Repository.MagazineRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,7 +16,7 @@ import java.util.stream.Collectors;
 public class MagazineService {
 
     private final MagazineRepository magazineRepository;
-    private final MagagineMapper modelMapper = new MagagineMapper();
+    private final MagazineMapper modelMapper = new MagazineMapper();
     public MagazineService(MagazineRepository magazineRepository) {this.magazineRepository = magazineRepository;}
 
     public MagazineDTO getMagazine(Long id) {
@@ -33,9 +34,21 @@ public class MagazineService {
         return magazine.getId();
     }
 
+    @Transactional
     public Long deleteMagazine(Long id) {
         magazineRepository.deleteById(id);
         return id;
+    }
+
+    @Transactional
+    public Long editMagazine(Magazine magazine) {
+        Optional<Magazine> oldMagazine = magazineRepository.findById(magazine.getId());
+        oldMagazine.get().setLgId(magazine.getLgId());
+        oldMagazine.get().setMdId(magazine.getMdId());
+        oldMagazine.get().setSmId(magazine.getSmId());
+        oldMagazine.get().setContent(magazine.getContent());
+
+        return magazine.getId();
     }
 
 }
